836b8ed22b8ed6fc67d2bbe817b4db2f8305540a
centinela1024tb <centinela1024tb@gmail.com>
2022-07-25 18:08:11 -0500

    [ADD] Se implementa class-validator | class-transformer | @nestjs/mapped-types
    => class-validator se configura en el controller para usar los decorators @
    => ValidationPipe se configura en main.ts
       impor: { ValidationPipe } from '@nestjs/common';
       usamos: app.useGlobalPipes(new ValidationPipe)
    => @nestjs/mapped-types se configura en el DTOs
       import: import { PartialType } from '@nestjs/mapped-types'
       usamos: export class UpdateProductDto extends PartialType(CreateProductDto) {}

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,45 @@
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types'
+
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+// es como la habiamos declarado
+// export class UpdateProductDto {
+//   readonly name?: string;
+//   readonly description?: string;
+//   readonly price?: number;
+//   readonly stock?: number;
+//   readonly image?: string;
+// }
+
+// es usando @nestjs/partial_type
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
